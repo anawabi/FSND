2b73ab470a5d455113a4a7798192e9b31fbe54d0
Aman Nawabi <aman.nawabi@gmail.com>
2021-02-14 20:19:27 -0800

    Added the get_questions endpoint

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -12,7 +12,105 @@ def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   setup_db(app)
+  # CORS(app, resources={r"*/api/*": {"origins": "*"}})
+  CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+    return response
+
+  QUESTIONS_PER_PAGE = 10
+
+  # Paginates the items recieved in the parameter items
+  def paginate_questions(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page-1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE 
+    question = formatted_questions = [question.format() for question in selection]
+    current_question = question[start:end]
+    return current_question
+
+  # Error Handling
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      "success": False, 
+      "error": 404,
+      "message": "Not found"
+    }), 404
+
+# -------------------------------------------------------------
+# Categories
+# -------------------------------------------------------------
+
+  @app.route('/categories')
+  def get_categories():
+    # Retrives all categories
+    category = Category.query.order_by(Category.id).all()
+    formatted_categories = {category.format() for category in categories}
+    
+    return jsonify({
+      'success': True,
+      'categories': formatted_categories
+    })
+
+# ------------------------------------------------------------
+#  Questions 
+# --------------------------------------------------------------  
+  @app.route('/questions', methods=['GET'])
+  def get_paginated_questions():
+    """GET a list of paginated questions"""
+    questions = Question.query.all()
+    # formatted_questions = [question.format() for question in questions]
+    current_questions = paginate_questions(request, questions)
+
+    categories = {category.id: category.type for category in Category.query.all()}
+
+    if len(current_questions) == 0:
+      abort(404)
+    
+    return jsonify({
+      'success': True,
+      'questions': current_questions,
+      'total_questions': len(questions),
+      'categories': categories,
+      'current_category': None
+    })
   
+  
+  @app.route('/questions', methods=['POST'])
+  def add_question():
+    """POST new question to the database"""
+    request_data = request.get_json()
+
+    question = request_data['question']
+    answer = request_data['answer']
+    difficulty = request_data['difficulty']
+    category = request_data['category']
+
+    repeated_question = Question.query.filter(Question.question == question).one_or_none()
+
+    if repeated_question is None:
+      try:
+        new_question = Question(
+          question=question,
+          answer=answer,
+          difficulty=difficulty,
+          category=category
+        )
+        new_question.insert()
+      except:
+        abort(500)
+      finally:
+        return jsonify({
+          'success': True
+        })
+    else:
+      abort(422)
+  
+ 
   '''
   @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
   '''
@@ -101,4 +199,31 @@ def create_app(test_config=None):
   
   return app
 
-    
+
+
+
+  # Without the pagenation using separate method for pagination
+  # @app.route('/questions', methods =['GET'])
+  # #  @cross_origin()
+  # def get_questions():
+  #   # page = request.args.get('page', 1, type=int)
+  #   question = Question.query.order_by(Question.id).all()
+  #   categories = Category.query.order_by(Category.id).all()
+  #   current_question = paginate(request, question)
+  #   formatted_categories = [category.format() for category in categories]
+
+  #   if len(current_question) == 0:
+  #     abort(404)
+
+  #   return jsonify({
+  # 		'success':True,
+  # 		'questions': current_question,
+  # 		'total_questions': len(current_question),
+  # 		'categories': formatted_categories
+  # 		})
+
+    # return jsonify({
+    #   'success': True,
+    #   'questions': current_question,
+    #   'total-questions': len(Question.query.all())
+    # })
